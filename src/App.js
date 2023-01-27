202775136538433562674610c6268f893b806d58
Muhammed Furkan <mfyilmaz.hukuk@gmail.com>
2023-01-27 21:06:25 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TodosPage from "./pages/todos-page";
+import CommentsPage from "./pages/comments-page";
+import ContactPage from "./pages/contact-page";
+import TodoHeader from "./components/todo-header/todo-header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <TodoHeader title="TODO Application" />
+      <Routes>
+        <Route path="/" element={<TodosPage />} />
+        <Route path="/comments" element={<CommentsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
